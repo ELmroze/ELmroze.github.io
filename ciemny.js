1c3c3ef29fd8acfb9a7320834bbc4d0f5364d042
ELmroze <113554158+ELmroze@users.noreply.github.com>
2022-09-14 22:43:12 +0200

    added new files

---
+++ ciemny.js
@@ -0,0 +1,38 @@
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('dark-theme');
+
+const darkModeToggle = document.querySelector('#tryb');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('dark-theme');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('dark-theme', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('dark-theme');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('dark-theme', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('dark-theme');
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode();
+  }
+});
